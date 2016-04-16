9b60b0c0a4f846b7ee6c2514410be7773d9dfc9c
HsuChingChen <chnbohwr@gmail.com>
2016-04-16 15:40:33 +0800

    fix error

--- sqlconfig.js
+++ sqlconfig.js
@@ -3,7 +3,7 @@ var data = {
 	user:process.env.OPENSHIFT_MYSQL_DB_USERNAME,
 	password:process.env.OPENSHIFT_MYSQL_DB_PASSWORD,
 	host:process.env.OPENSHIFT_MYSQL_DB_HOST,
-    port:process.env.OPENSHIFT_MYSQL_DB_PORT
+    port:process.env.OPENSHIFT_MYSQL_DB_PORT,
 	dialect: 'mysql'
 };
 
